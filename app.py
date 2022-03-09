c84e25a2aa700c9c451cdb4ebee2ab5c8c293a6f
Narendra G O <60711014+narendrago@users.noreply.github.com>
2022-03-09 13:31:52 +0530

    Integrations done

--- app.py
+++ app.py
@@ -3,5 +3,19 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/aboutus',methods=['GET'])
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/contactus',methods=['GET','POST'])
+def contactus():
+    return render_template('contactus.html')
+
+
+@app.route('/map',methods=['GET','POST'])
 def map():
     return render_template('map.html')
